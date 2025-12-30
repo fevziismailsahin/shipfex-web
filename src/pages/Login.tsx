80f4711c818673d2439ec1d5c27acb42d1c8251b
fevziismailsahin <fevziismailsahin@gmail.com>
2025-12-29 20:05:13 -0700

    update

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Package, ArrowRight, Lock, Mail } from 'lucide-react';
+import { ArrowRight, Lock, Mail } from 'lucide-react';
 
 export default function Login() {
   const navigate = useNavigate();
